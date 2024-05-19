796d063c73117906ed4212a125040a3288b06cb6
FelipePenagosss <andres.penagos01@uptc.edu.co>
2024-05-19 17:50:25 -0500

    Se incluyo el procedimiento que tiene el metodo

--- MetodoSecante.py
+++ MetodoSecante.py
@@ -14,3 +14,14 @@ xi = 1
 xnuevo=0
 error = 0.0001
 
+#Parte de procedimiento
+tramo = 1
+tabla=[]
+iteracion=-1
+while (error<tramo):
+    iteracion=iteracion+1
+    xnuevo = xi-(fdx(xi) * (xi - xiMenos)) / (fdx(xi) - fdx(xiMenos))
+    tramo  = abs(xnuevo-xi)
+    xiMenos = xi
+    xi = xnuevo
+    tabla.append([iteracion,xi,tramo])
